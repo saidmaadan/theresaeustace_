5d36de67d6ab0f5e0e9bb182867735c26853b39b
Said Maadan <saidmaadan@gmail.com>
2025-01-07 11:43:37 -0600

    Updated use-toast

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -1,5 +1,5 @@
 "use client"
-
+// @ts-ignore
 // Inspired by react-hot-toast library
 import * as React from "react"
 
@@ -18,7 +18,7 @@ type ToasterToast = ToastProps & {
   action?: ToastActionElement
 }
 
-const actionTypes = {
+export const actionTypes = {
   ADD_TOAST: "ADD_TOAST",
   UPDATE_TOAST: "UPDATE_TOAST",
   DISMISS_TOAST: "DISMISS_TOAST",
